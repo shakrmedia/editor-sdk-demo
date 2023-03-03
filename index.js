760ddc0ac17ad2871f47c9e61e9ac70114b7ba68
Jeonghwan Lee <jeonghwan@sha.kr>
2023-03-03 22:28:08 +0900

    Add aspect ratio variation demo (#4)

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const express = require('express');
 const ShakrAPI = require('./lib/shakr-api');
 const generateCertificate = require('./lib/certificate');
 
-const SHAKR_TEMPLATE_STYLE_VERSION_ID = '8KX3s4';
+const SHAKR_TEMPLATE_STYLE_VERSION_ID = 'o08dHB';
+const SHAKR_ASPECT_RATIO_VARIATON_VERSION_ID = 'n9rysW';
 
 const options = generateCertificate();
 const app = express();
@@ -21,6 +22,18 @@ app.post('/api/videos', async (req, res) => {
     const edit_token_obj = await shakrAPI.createEditToken(SHAKR_TEMPLATE_STYLE_VERSION_ID, req.body);
 
     res.json(edit_token_obj);
+});
+
+app.post('/api/videos/aspect_ratio_variation', async (req, res) => {
+    const render_session = await shakrAPI.generateAspectRatioVariation(req.body.render_session_id, SHAKR_ASPECT_RATIO_VARIATON_VERSION_ID);
+
+    res.json(render_session);
+});
+
+app.get('/api/videos/:id', async (req, res) => {
+    const render_session = await shakrAPI.getRenderSession(req.params.id);
+
+    res.json(render_session);
 });
 
 require('https').createServer(options, app).listen(3000, () => {
